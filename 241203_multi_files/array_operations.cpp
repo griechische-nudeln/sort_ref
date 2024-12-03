4cedb012ba022a423a9d66438cec775755c67ef1
griechische-nudeln <latifakhamatullina@gmail.com>
2024-12-03 12:09:01 +0300

    Refactor repo structure

--- 241203_multi_files/array_operations.cpp
+++ 241203_multi_files/array_operations.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 #include <algorithm>
 
-void swap_function(int A[], int size) {
+void hlr::swap_function(int arr[], const int size) {
     for (int i = 0; i < size - 2; i++) {
-        if ((A[i] < A[i + 1] && A[i + 1] < A[i + 2]) || (A[i] > A[i + 1] && A[i + 1] > A[i + 2])) {
-            std::swap(A[i + 1], A[i + 2]); 
+        if ((arr[i] < arr[i + 1] && arr[i + 1] < arr[i + 2]) || (arr[i] > arr[i + 1] && arr[i + 1] > arr[i + 2])) {
+            std::swap(arr[i + 1], arr[i + 2]); 
         }
     }
 }
 
-void print_array(const int arr[], const int size, const std::string& message) {
+void hlr::print_array(const int arr[], const int size,  const char* const message) {
     std::cout << message << "\n";
     for (int i = 0; i < size; i++) {
         std::cout << arr[i] << " ";
--- 241203_multi_files/script.bat
+++ 241203_multi_files/script.bat
@@ -1,3 +1,7 @@
 @echo off
+chcp 65001 > log
+del log
 
 g++ -std=c++17 -o program.exe main.cpp array_operations.cpp
+
+program.exe
